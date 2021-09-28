3b3e40f8eabc762f0613633071eb76d376472786
Twixer10 <tom.26.tec@hotmail.com>
2021-09-28 09:34:38 +0200

    Add files via upload

---
+++ looneytised.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+grep --extended-regexp "theo1|steven1|arnaud1|pierre-jean" |  sed "s/theo1/Wile E. Coyote/g" | sed "s/steven1/Daffy Duck/g" | sed "s/arnaud1/Porky Pig/g" | sed "s/pierre-jean/Marvin the Martian/g"
